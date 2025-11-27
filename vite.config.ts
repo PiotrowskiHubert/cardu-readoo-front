b571212e67506877f018c45de6091e9405e62521
PiotrowskiHubert <hubert.piotrowski.kmwtw@gmail.com>
2025-11-27 16:08:20 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     },
   },
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -22,4 +23,7 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    port: 5173,
+  }
 })
